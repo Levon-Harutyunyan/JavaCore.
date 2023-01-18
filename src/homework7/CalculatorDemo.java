528a5429fc8801112d351d93c1de1e65180c54f3
levon Harutyunyan <levon2022levon.2@gmail.com>
2023-01-18 17:24:31 +0400

    Homework7

---
+++ src/homework7/CalculatorDemo.java
@@ -0,0 +1,14 @@
+package homework7;
+
+public class CalculatorDemo {
+            public static void main(String[] args) {
+                Calculator calculator = new Calculator();
+                calculator.sum(8.5, 7.2);
+                calculator.sum(5, 8);
+                calculator.multiply(2, 4);
+                calculator.divade(7, 7);
+
+            }
+        }
+
+
