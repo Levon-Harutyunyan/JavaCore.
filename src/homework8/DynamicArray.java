e9e57185a45d36d7ad494432673541c397327c08
levon Harutyunyan <levon2022levon.2@gmail.com>
2023-01-24 18:09:45 +0400

    Homework9

--- src/homework8/DynamicArray.java
+++ src/homework8/DynamicArray.java
@@ -9,22 +9,22 @@ public class DynamicArray {
     //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
     //և ավելացնենք
     public void add(int value) {
-        if (size== array.length){
+        if (size == array.length) {
             extend();
         }
-        array[size++]= value;
+        array[size++] = value;
     }
 
     //1․ ստեղծել հին մասիվից 10 էլեմենտ ավելի մեծ մասիվ
     //2․ քցել հին մասիվի էլեմենտները նորի մեջ
     //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
     private void extend() {
-        int[] tmp=new int[array.length +10];
-        for (int i = 0; i <size; i++) {
-            tmp[i] =array[i];
+        int[] tmp = new int[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            tmp[i] = array[i];
 
         }
-        array=tmp;
+        array = tmp;
     }
 
 
@@ -40,12 +40,58 @@ public class DynamicArray {
     //տպել մասիվի ավելացված էլեմենտները
     public void print() {
         for (int i = 0; i < size; i++) {
-            System.out.print(array [i]+" ");
+            System.out.print(array[i] + " ");
 
         }
     }
-}
 
+    public boolean isEmpty() {
+        if (array.length < 0) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public int getFirstIndexByValue(int value) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (index > array.length) {
+                return 0;
+            } else if (array[i] == value) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public void set(int index, int value) {
+        for (int i = 0; i < size; i++) {
+            if (index == i) {
+                array[i] = value;
+            }
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public void add(int index, int value) {
+        for (int i = index; i <= size; i++) {
+            int tmp = value;
+            value = array[i];
+            array[i] = tmp;
+        }
+        size++;
+    }
+
+    public void delete(int index) {
+
+        for (int i = index + 1; i < size; i++) {
+            array[i - 1] = array[i];
+        }
+        size--;
+    }
+}
 
 
 
